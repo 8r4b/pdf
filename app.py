4bcb209cceddefdc6ed034f620aa862bcef2e530
Mohamed <mohamedalsaedi1999@gmail.com>
2025-06-27 16:09:01 +0300

    Fix: stable requirements for Streamlit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings   # Updated import
+from langchain.chat_models import ChatOpenAI               # Updated import
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain_community.vectorstores import Chroma
+from langchain.vectorstores import Chroma                   # Updated import
 from htmlTemplates import css, bot_template, user_template
 
 import fitz  # PyMuPDF
@@ -55,8 +56,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", return_messages=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
@@ -71,11 +71,9 @@ def handle_userinput(user_question):
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
